09ff78cf47c9aeb99722f164e1ec08fae9f5156f
advanceuser <luisalbertorg117@gmail.com>
2024-07-17 18:50:08 -0600

    Initial commit

---
+++ src/main/java/com/luis/curso/springboot/calendar/interceptor/SpringbootHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.luis.curso.springboot.calendar.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/curso/springboot/calendar_interceptor/SpringbootHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.curso.springboot.calendar_interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
